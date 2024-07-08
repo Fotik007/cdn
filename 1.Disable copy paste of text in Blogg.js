8ea2c263e67c5cb6568c5d85962c617c6585061a
Fotik007 <142956951+Fotik007@users.noreply.github.com>
2024-07-08 15:47:06 +0530

    Create 1.Disable copy paste of text in Blogg.js

---
+++ 1.Disable copy paste of text in Blogg.js
@@ -0,0 +1,8 @@
+<script type='text/javascript'>
+if (typeof document.onselectstart != "undefined") {
+document.onselectstart = new Function("return false");
+} else {
+document.onmouseup = new Function("return false");
+document.onmousedown = new Function("return false");
+}
+</script>
