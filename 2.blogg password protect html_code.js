4a3cfb0d2d0823f295d2d99b9b9cd0ff9b8af196
Fotik007 <142956951+Fotik007@users.noreply.github.com>
2024-07-08 17:18:45 +0530

    2.blogg password protect html_code.js

--- 2.blogg password protect html_code.js
+++ 2.blogg password protect html_code.js
@@ -1,6 +1,6 @@
 <script type="text/javascript">
 var password = prompt('Enter the password to view this content:', '');
-if (password != '7204') {
+if (password != '🇮🇳') {
     alert('Incorrect password! Please try again.');
     window.location.href = '🇮🇳';
 }
