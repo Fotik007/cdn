8803ff2b7dbc387c1c5ffdef2fe8ddde8914be14
Fotik007 <142956951+Fotik007@users.noreply.github.com>
2024-07-08 17:16:09 +0530

    Create 2.blogg password protect html code.js

---
+++ 2.blogg password protect html code.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+var password = prompt('Enter the password to view this content:', '');
+if (password != '7204') {
+    alert('Incorrect password! Please try again.');
+    window.location.href = '🇮🇳';
+}
+</script>
